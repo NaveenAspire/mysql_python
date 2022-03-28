0bbd1558e61e4b3e309b3ad409e3f3e200b63635
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-03-28 18:45:01 +0530

    Updated 28-03-2022

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,16 +1,21 @@
 """"This module that is used for pull data from mysql database and make it as as json file then upload into s3"""
-import json
 import os
 import mysql.connector
 from s3 import S3Service
+import configparser
+import argparse
 import pandas as pd
+
+config = configparser.ConfigParser()
+config.read('mysql_config.ini')
 
 class MysqlData:
     """"This class contains method for get data from mysql and upload into s3 as json file"""
-    def __init__(self):
+    def __init__(self,bucket_name):
         """"This is the init method of MysqlData class"""
-        self.bucket_name = 'aspire-mysql-python'
-        self.con = mysql.connector.connect(user = 'root', password = '123',host='localhost',database = "employeedb")
+        self.bucket_name = bucket_name
+        self.con = mysql.connector.connect(user = config['MySQL']['user'], password = config['MySQL']['password'],
+                                           host=config['MySQL']['host'],database = config['MySQL']['database'])
         print(self.con)
         
         
@@ -33,7 +38,7 @@ class MysqlData:
         file_path = data_path+"/"+file_name
         data_as_df.to_json(file_path, orient='records', lines= True)
         key = 'mysql_python/source/'+file_name
-        self.upload_json_to_s3(file_path,self.bucket_name,key)
+        # self.upload_json_to_s3(file_path,self.bucket_name,key)
         
         
     def get_csv_file(self,data_as_df):
@@ -45,7 +50,7 @@ class MysqlData:
         file_path = data_path+"/"+file_name
         data_as_df.to_csv(file_path,index=False)
         key = 'mysql_python/stage/'+file_name
-        self.upload_json_to_s3(file_path,self.bucket_name,key)
+        # self.upload_json_to_s3(file_path,self.bucket_name,key)
     
     
     def upload_json_to_s3(self,file,bucket_name,key):
@@ -54,8 +59,13 @@ class MysqlData:
         s3.upload_file_to_s3(file,bucket_name,key)
     
 def main():
-    """This is the main method for the module mysql_connection"""    
-    mysql_data = MysqlData()
+    """This is the main method for the module mysql_connection"""  
+    parser = argparse.ArgumentParser("For giving bucket name to store files fromsftp server")
+    parser.add_argument('--bucket_name', type=str,
+                        help='Enter th bucket name for store files retrived from sftp server', required=True)
+    args = parser.parse_args()
+    print(args.bucket_name)
+    mysql_data = MysqlData(args.bucket_name)
     json_data = mysql_data.get_mysql_data()
     mysql_data.get_json_file(json_data)
     mysql_data.get_csv_file(json_data)
